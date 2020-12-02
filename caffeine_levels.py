690995ac7ba06a6c4fa1fd07e78338b0e6f2ecc2
TR <teddy@teddyrowan.com>
2020-12-01 20:56:58 -0500

    Script now supports taking multiple pills and allows the user to define those times (@ compile time).

--- caffeine_levels.py
+++ caffeine_levels.py
@@ -4,9 +4,9 @@ Author: Teddy Rowan
 Last Modified: December 1, 2020
 Description: Numerical PDE simulation of caffeine levels in blood from taking caffeine pills.
 
-TODO: implement caffeine absorption function
-TODO: implement caffeine depletion function
-TODO: implement a list of caffeine pill taking times + adjustments for when to increase levels
+TODO: define constants (time, caffeine pill values, time_delay, etc.)
+TODO: extend simulation to model cup of coffee.
+TODO: implement ML to optimize pill taking time.
 
 Notes: 
 - Caffeine elimination half-life is 3-5 hours. [1]
@@ -34,24 +34,28 @@ time_list = np.array([])
 pill_list = np.array([])
 caff_list = np.array([])
 
-#TODO: create a time_intake list for when pills are taken. 
+time_delay = 5
+pill_time = np.array([0, 60, 300, 500]) + time_delay
 
 time_wake = 0 
-# Assume 9am for now
-
-time_sleep = 15*60 
-# Assume midnight for now
+time_sleep = 16.5*60
+# Assumes 8.30am to 1am = (3.5+12+1 hours)
 
 time_step = 1
 
-caff_pill_level = 100
+caff_pill_level = 0
 # Initial caffeine level in stomach
 
 caff_blood_level = 0
 # Initial caffeine level in bloodstream
 
-for seg in range(time_wake, time_sleep, time_step):
+for seg in range(time_wake, int(time_sleep), time_step):
     time_list = np.append(time_list, seg)
+    
+    # If it's time to take a pill
+    if (pill_time.size > 0 and seg == pill_time[0]):
+        caff_pill_level = caff_pill_level + 100
+        pill_time = np.delete(pill_time, 0)
     
     caff_depl = caff_depletion(caff_blood_level)
     caff_absp = caff_absorp(caff_pill_level)
@@ -59,13 +63,12 @@ for seg in range(time_wake, time_sleep, time_step):
     caff_pill_level = caff_pill_level - caff_absp
     caff_blood_level = caff_blood_level + caff_absp - caff_depl
 
-    # Do calculations here. to solve value to add to pill_list and caff_list
     pill_list = np.append(pill_list, caff_pill_level)
     caff_list = np.append(caff_list, caff_blood_level)
     
 
 fig = plt.figure()
-plt.plot(time_list, pill_list, 'ro-', markersize=3)
+plt.plot(time_list, pill_list, 'r-', markersize=1)
 plt.xlabel("Time since waking [minutes]")
 plt.ylabel("Stomach-caffeine level [mg]")
 plt.title("Stomach-Caffeine Simulation")
@@ -73,7 +76,7 @@ plt.grid()
 plt.show(block=False)
 
 fig = plt.figure()
-plt.plot(time_list, caff_list, 'ro-', markersize=3)
+plt.plot(time_list, caff_list, 'r-', markersize=1)
 plt.xlabel("Time since waking [minutes]")
 plt.ylabel("Blood-caffeine level [mg]")
 plt.title("Blood-Caffeine Simulation")
