8659d577ce8720213398be5ff99c56c3fe247356
TR <teddy@teddyrowan.com>
2020-12-11 16:37:47 -0500

    Fixed a memory bug where plot data wasn't clearing and was crashing overnight simulations. Abstracted ML settings to another file. Automated gif making.

--- driver_machinelearning.py
+++ driver_machinelearning.py
@@ -4,9 +4,8 @@ Author: Teddy Rowan
 Last Modified: December 10, 2020
 Description: Driver function for ML optimization of caffeine blood-concentration simulations. 
 
-TODO: abstract settings to another file.
 TODO: abstract helper code to another file.
-TODO: export results images to a gif.
+TODO: abstract mutation code.
 TODO: mutation code just isn't good.
     Keep top 5% of sims.
         - Let's say 10% chance of totally new sim
@@ -23,6 +22,7 @@ from caffeine_levels import CaffeineLevels
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import settings_ml as sml
 
 def generate_schedule(n_pills, max_time):
     sched = np.array([0]) # dope the schedule to take a pill at t=0
@@ -30,7 +30,7 @@ def generate_schedule(n_pills, max_time):
     
     while sched.size < n_pills:
         val = round(random.uniform(0, 1)*max_time)
-        while val in sched:
+        while val in sched: # avoid duplicates
             val = val + 1
         
         sched = np.append(sched, val)
@@ -48,7 +48,7 @@ def mutate(old, comparison):
         
 
         if (random.uniform(0,1) < 0.1): #1/10 chance, give something a totally new value
-            val = round(random.uniform(0, 1)*day_length)
+            val = round(random.uniform(0, 1)*settings['day_length'])
 
         # Make sure values are positive and unique.
         should_change = True
@@ -71,43 +71,42 @@ def print_fitness(arr, pop_size, print_all):
         print('Worst Fitness: '+ str(arr[-1].fitness))
     
 # TODO: add legend
-def plot_all(data, name, display):
+def plot_all(data, gen, display):
     plt.rcParams['figure.figsize'] = (8, 5)
-    fig = plt.subplots()
+    fig = plt.figure()
+
     for sim in data:
-        blood = plt.plot(sim.time_list, sim.caff_list, 'r-', alpha=0.08)
-    
-    plt.plot(data[0].time_list, data[0].caff_list, 'b-')
-    plt.plot(sim.time_list, sim.opt, 'g-')
+        blood = plt.plot(sim.time_list, sim.caff_list, 'r-', alpha=settings['alpha']) #0.08 for 250pop
+
+    aa = plt.plot(data[0].time_list, data[0].caff_list, 'b-', linewidth=2)
+    bb = plt.plot(sim.time_list, sim.opt, 'g--', linewidth=2)
     
     plt.xlabel("Time since waking [minutes]")
     plt.ylabel("Blood-caffeine level [mg]")
-    plt.title("Blood-Caffeine Simulation")
-    plt.xlim([0, day_length])
+    plt.title("Blood-Caffeine Simulation. Gen: [" + str(gen) + "]")
+    plt.xlim([0, settings['day_length']])
     plt.ylim([0, 165])
             
     plt.grid()
+    name = "r" + "{:03d}".format(gen) + "_blood.png"
     plt.savefig('./screens/' + name)
     if (display):
         plt.show()
-    plt.close()
+   
+    # Fix memory issues
+    fig.clear()
+    plt.close(fig)
+    del aa
+    del bb
+    del blood
 
 
-# okay, do the ML Simulation.
-day_length = 15*60 #((12-8.5) + (12) + 0.5)*60 #16hrs
-pill_strength = 50#100#50#100
-optimal_caffeine = 120
-night_caffeine = 20
-generations = 50
-
-population = 250#100#250
-pill_count = 6#3
-
+settings = sml.get_settings()
 pop_arr = np.array([])
 
-for ii in range(0, population):
-    schedule = generate_schedule(pill_count, day_length)
-    caff = CaffeineLevels(day_length, pill_strength, schedule, optimal_caffeine, night_caffeine)
+for ii in range(0, settings['population']):
+    schedule = generate_schedule(settings['pill_count'], settings['day_length'])
+    caff = CaffeineLevels(settings['day_length'], settings['pill_strength'], schedule, settings['optimal_caffeine'], settings['night_caffeine'])
     fit = caff.run_simulation()
     pop_arr = np.append(pop_arr, caff)
 
@@ -117,27 +116,26 @@ pop_arr = sorted(pop_arr, key=lambda x:x.fitness)
 print(pop_arr[0].pill_schedule)
 # Print the schedule for the best profile
 
-print_fitness(pop_arr, population, False)
+print_fitness(pop_arr, settings['population'], False)
 # Print best/worst/median fitness values
 
-plot_all(pop_arr, 'r0_blood.png', False)
+plot_all(pop_arr, 0, False)
 
 # save the top 5, iterate through the rest w/ probability fitness[x]/fitness[-1] of mutating
 # if mutating: randomly choose one from top 25%, take the avg value for each data point, then add a random change to it
-for jj in range(5, population):
+for jj in range(5, settings['population']):
     if (random.uniform(0,1) < pop_arr[jj].fitness/pop_arr[-1].fitness):
-        transform_index = round(random.uniform(0,1)*population*0.25)
-        
+        transform_index = round(random.uniform(0,1)*settings['population']*0.25)
         new_sched = mutate(pop_arr[jj].pill_schedule, pop_arr[transform_index].pill_schedule)
         pop_arr[jj].pill_schedule = new_sched
 
 sched = pop_arr[0].pill_schedule
-for counter in range(1,generations+1):
+for counter in range(1,settings['generations']+1):
     print('Repeating for gen: ' + str(counter))
     
     pop_arr2 = np.array([])
-    for ii in range(0, population):
-        caff = CaffeineLevels(day_length, pill_strength, pop_arr[ii].pill_schedule, optimal_caffeine, night_caffeine)
+    for ii in range(0, settings['population']):
+        caff = CaffeineLevels(settings['day_length'], settings['pill_strength'], pop_arr[ii].pill_schedule, settings['optimal_caffeine'], settings['night_caffeine'])
         fit = caff.run_simulation()
         pop_arr2 = np.append(pop_arr2, caff)
     
@@ -147,15 +145,15 @@ for counter in range(1,generations+1):
     if (not np.array_equal(sched, pop_arr2[0].pill_schedule)):
         sched = pop_arr2[0].pill_schedule        
         print(pop_arr2[0].pill_schedule)
-        print_fitness(pop_arr2, population, False)
+        print_fitness(pop_arr2, settings['population'], False)
 
-    title = 'r' + str(counter) + '_blood.png'
-    plot_all(pop_arr2, title, False)
+    #title = 'r' + str(counter) + '_blood.png'
+    plot_all(pop_arr2, counter, False)
     
     # Mutate the data
-    for jj in range(5, population):
+    for jj in range(5, settings['population']):
         if (random.uniform(0,1) < pop_arr2[jj].fitness/pop_arr2[-1].fitness):
-            transform_index = round(random.uniform(0,1)*population*0.25)
+            transform_index = round(random.uniform(0,1)*settings['population']*0.25)
             new_sched = mutate(pop_arr2[jj].pill_schedule, pop_arr2[transform_index].pill_schedule)
             pop_arr2[jj].pill_schedule = new_sched
     
---
+++ settings_ml.py
@@ -0,0 +1,37 @@
+"""
+settings_ml.py
+Author: Teddy Rowan
+Last Modified: December 10, 2020
+Description: Settings for the machine-learning driver function for the caffeine simulations.
+"""
+
+def get_settings():
+    settings = {}
+
+    settings['day_length'] = 15*60
+    # Common: [15*60, ((12-8.5)+12+0.5)*60 = 16*60]
+    
+    settings['pill_strength'] = 100
+    # Common: 50mg, 100mg
+    
+    settings['pill_count'] = 3
+    # How many pills/half-pills to take
+    
+    settings['optimal_caffeine'] = 120
+    # Desired caffeine during daytime hours [mg]
+    
+    settings['night_caffeine'] = 20
+    # Acceptable caffeine level at bedtime [mg]
+        
+    settings['population'] = 250
+    # How many sims in each generation
+    
+    settings['generations'] = 50
+    # How many generations of simulations to run
+    
+    #TODO: add alpha setting
+    settings['alpha'] = 0.08
+    # 0.08 for 250 pop
+    # 0.03 for 5000 pop
+    
+    return settings
