cd94dcc53a336dfb95af5580f2b6d5ccda6711b3
TR <teddy@teddyrowan.com>
2020-12-12 15:25:19 -0500

    Added usage instructions in README.

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# Blood-Caffeine Levels Simulation
+# Blood-Caffeine Simulations
 
-The goal of this script is to use the Euler Method to step through the DEs for caffeine absorption and depletion to optimize the maintenance of your caffeine levels throughout the day.
+These scripts simulate the breakdown of caffeine pills in your stomach, absorption of caffeine into your bloodstream, and its dissipation over time. There are two driver scripts for the class that allow the user to either input a pill schedule and see the resulting caffeine profiles or to define optimal daytime/nighttime caffeine levels and how many pills they want to take and then use machine learning (genetic algorithm) to find the optimal time to take their pills.
 
+
+![3 Pill Gif](./screens/3x100-50gen-250pop_run2.gif "3x100mg Pills")
+
+### Usage Example
+
+* Open the driver_user.py script define your pill schedule and paramaters and then run the script. The script will analyze the data and create a figure for your stomach-caffeine levels and another for your blood-caffeine levels.
+
+-OR-
+
+* Alternatively, open and modify the settings_ml.py file, define your paramaters, and run the script. In time the script will optimize your pill schedule to figure out when you should be taking them.
 
 ### Author
 * Teddy Rowan
-
--- driver_user.py
+++ driver_user.py
@@ -1,31 +1,46 @@
 """
 driver_user.py
 Author: Teddy Rowan
-Last Modified: December 3, 2020
+Last Modified: December 12, 2020
 Description: Driver function for user-created caffeine blood-concentration simulations. 
 
 """
 
 from caffeine_levels import CaffeineLevels
 import numpy as np
-    
 
-day_length = 15*60
 pill_strength = 50
+# What is the strength [mg] of the caffeine pills
+    
+day_length = 15*60
+# How long is your day [minutes]
+
+daytime_optimal = 120
+# What is your optimal daytime caffeine level [mg]
 
 night_level = 20
-optimal = 120
+# What is your comfortable bedtime caffeine level [mg]
+
+pill_schedule = np.array([0, 1, 2, 126, 238, 347])
+# Best profile for 50mg pills (via ML) [times in minutes since waking]
 
 
-##pill_schedule = np.array([0, 60, 300]) #100mg pills
-#pill_schedule = np.array([0, 1, 2, 120, 240, 360]) # 50mg pills
-#pill_schedule = np.array([0, 1, 90, 230, 370]) # 50mg pills
+""" 
+Recommended pill schedules
 
-#pill_schedule = np.array([0, 77, 306]) # 100mg (best to date)
-#pill_schedule = np.array([0, 20, 21, 129, 285, 352]) # 50mg (best to date)
-pill_schedule = np.array([0, 1, 2, 135, 240, 365]) # 50mg (user best)
-optimal = 120
+pill_schedule = np.array([0, 1, 301])
+# Best profile for 100mg pills (via ML)
 
-caff = CaffeineLevels(day_length, pill_strength, pill_schedule, optimal, night_level)
+pill_schedule = np.array([0, 1, 2, 126, 238, 347])
+# Best profile for 50mg pills (via ML)
+
+pill_schedule = np.array([0, 60, 300])
+# Teddy's standard 100mg schedule
+"""
+
+# Initialize and run the simulation
+caff = CaffeineLevels(day_length, pill_strength, pill_schedule, daytime_optimal, night_level)
 fit = caff.run_simulation()
 print("Fitness: " + str(fit))
+caff.plot_stomach(False)
+caff.plot_results()
